ece1996b67bd5dd063c1bc6aa79bb4179e3b88c8
Abdul Wasay Mudassir <133111076+Abdulwasay551@users.noreply.github.com>
2025-07-26 09:34:14 +0500

    Update urls.py

--- setting/urls.py
+++ setting/urls.py
@@ -49,6 +49,10 @@ urlpatterns = [
     path('analytics/', include('analytics.urls')),
 ]
 
+# Serve static files in development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
