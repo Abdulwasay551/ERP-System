0d840dcd30b2c7a303f84a0c3cd243bcc6357f17
Abdul Wasay Mudassir <133111076+Abdulwasay551@users.noreply.github.com>
2025-08-19 05:09:57 +0500

    Updated wsgi settings

--- setting/wsgi.py
+++ setting/wsgi.py
@@ -18,7 +18,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'setting.settings')
 
 # Check if we're in a Vercel environment and static files need to be collected
-if os.environ.get('VERCEL') == True:
+if os.environ.get('VERCEL'):
     try:
         # Get the project directory
         project_dir = Path(__file__).parent.parent
